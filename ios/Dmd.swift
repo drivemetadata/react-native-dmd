1b67da7ae0e0c9c6940ac65133a485a22bf8f040
Ranjeet Ranjan <ranjeet@adsizzler.com>
2024-12-14 19:20:59 +0530

    chore: initial commit

---
+++ ios/Dmd.swift
@@ -0,0 +1,8 @@
+@objc(Dmd)
+class Dmd: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
